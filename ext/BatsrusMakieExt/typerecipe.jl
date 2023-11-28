aaad4714a26743313cbb9294e6c11da3b33f57b0
Hongyang Zhou <hyzhou@umich.edu>
2023-11-28 11:49:27 -0500

    Adjust for Makie 0.20

--- ext/BatsrusMakieExt/typerecipe.jl
+++ ext/BatsrusMakieExt/typerecipe.jl
@@ -17,7 +17,7 @@ function Makie.convert_arguments(P::Makie.PointBased, bd::BATLData, var::String)
 end
 
 "Conversion for 2D plots."
-function Makie.convert_arguments(P::Makie.SurfaceLike, bd::BATLData, var::String;
+function Makie.convert_arguments(P::Makie.GridBased, bd::BATLData, var::String;
    plotrange=[-Inf,Inf,-Inf,Inf], plotinterval=0.1)
    x, y, w = getdata(bd, var, plotrange, plotinterval)
 
