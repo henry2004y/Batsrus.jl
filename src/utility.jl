33e884808d0cc54c00eefca4323b8f3a3d497664
Hongyang Zhou <hyzhou@umich.edu>
2024-11-11 18:19:22 -1000

    Force type conversion

--- src/utility.jl
+++ src/utility.jl
@@ -13,10 +13,11 @@ data resolution is the same as the original.
 - `useMatplotlib=true`: Whether to Matplotlib (somehow faster) or NaturalNeighbours for scattered interpolation.
 """
 function interp2d(bd::BATS{2, 3, T}, var::AbstractString,
-   plotrange::Vector=[-Inf32, Inf32, -Inf32, Inf32], plotinterval::Real=Inf32;
+   plotrangeIn::Vector=[-Inf32, Inf32, -Inf32, Inf32], plotinterval::Real=Inf32;
    innermask::Bool=false, rbody::Real=1.0, useMatplotlib::Bool=true) where T
    x, w = bd.x, bd.w
    varIndex_ = findindex(bd, var)
+   plotrange = T.(plotrangeIn)
 
    local Wi::Union{Matrix{T}, Adjoint{T, Matrix{T}}}
 
