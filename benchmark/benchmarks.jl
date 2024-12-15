a808ab2703dddd95bf57ff831117b86ba794af92
Hongyang Zhou <hyzhou@umich.edu>
2024-12-15 18:08:57 -0500

    Remove one B benchmark

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -21,7 +21,6 @@ file = joinpath(directory, files[2])
 bd = load(file)
 SUITE["read"]["Load binary structured"] = @benchmarkable load($file)
 SUITE["read"]["Extract density"] = @benchmarkable Batsrus.getvar($bd, "Rho")
-SUITE["read"]["Extract B"] = @benchmarkable Batsrus.getvar($bd, "B")
 SUITE["read"]["Extract Bmag"] = @benchmarkable get_magnitude($bd, :B)
 SUITE["read"]["Interp2d"] = @benchmarkable Batsrus.interp2d($bd, "rho")
  
