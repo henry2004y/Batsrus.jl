56a0b088450efc99100bb764c8469726baeb5446
Hongyang Zhou <hyzhou@umich.edu>
2025-04-30 22:34:35 -0400

    Formatting

--- src/unit/batsrusmacro.jl
+++ src/unit/batsrusmacro.jl
@@ -1,5 +1,5 @@
 """
-    macro bu_str(unit)
+	 macro bu_str(unit)
 
 String macro to easily recall Batsrus units located in the `UnitfulBatsrus` package.
 Although all unit symbols in that package are suffixed with `_bu`, the suffix should not be
@@ -14,46 +14,46 @@ julia> 1.0bu"amucc"
 ```
 """
 macro bu_str(unit)
-   ex = Meta.parse(unit)
-   esc(replace_value(ex))
+	ex = Meta.parse(unit)
+	esc(replace_value(ex))
 end
 
 const allowed_funcs = [:*, :/, :^, :sqrt, :√, :+, :-, ://]
 function replace_value(ex::Expr)
-   if ex.head == :call
-      ex.args[1] in allowed_funcs ||
-         error("""$(ex.args[1]) is not a valid function call when parsing a unit.
-          Only the following functions are allowed: $allowed_funcs""")
-      for i=2:length(ex.args)
-         if typeof(ex.args[i])==Symbol || typeof(ex.args[i])==Expr
-            ex.args[i] = replace_value(ex.args[i])
-         end
-      end
-      return Core.eval(@__MODULE__, ex)
-   elseif ex.head == :tuple
-      for i=1:length(ex.args)
-         if typeof(ex.args[i]) == Symbol
-            ex.args[i] = replace_value(ex.args[i])
-         else
-            error("only use symbols inside the tuple.")
-         end
-      end
-      return Core.eval(@__MODULE__, ex)
-   else
-      error("Expr head $(ex.head) must equal :call or :tuple")
-   end
+	if ex.head == :call
+		ex.args[1] in allowed_funcs ||
+			error("""$(ex.args[1]) is not a valid function call when parsing a unit.
+			 Only the following functions are allowed: $allowed_funcs""")
+		for i ∈ 2:length(ex.args)
+			if typeof(ex.args[i])==Symbol || typeof(ex.args[i])==Expr
+				ex.args[i] = replace_value(ex.args[i])
+			end
+		end
+		return Core.eval(@__MODULE__, ex)
+	elseif ex.head == :tuple
+		for i ∈ 1:length(ex.args)
+			if typeof(ex.args[i]) == Symbol
+				ex.args[i] = replace_value(ex.args[i])
+			else
+				error("only use symbols inside the tuple.")
+			end
+		end
+		return Core.eval(@__MODULE__, ex)
+	else
+		error("Expr head $(ex.head) must equal :call or :tuple")
+	end
 end
 
 dottify(s, t, u...) = dottify(Expr(:(.), s, QuoteNode(t)), u...)
 dottify(s) = s
 
 function replace_value(sym::Symbol)
-   s = Symbol(sym, :_bu)
-   if !(isdefined(UnitfulBatsrus, s) && typecheck_bool(getfield(UnitfulBatsrus, s)))
-      error("Symbol $s could not be found in UnitfulBatsrus.")
-   end
+	s = Symbol(sym, :_bu)
+	if !(isdefined(UnitfulBatsrus, s) && typecheck_bool(getfield(UnitfulBatsrus, s)))
+		error("Symbol $s could not be found in UnitfulBatsrus.")
+	end
 
-   return getfield(UnitfulBatsrus, s)
+	return getfield(UnitfulBatsrus, s)
 end
 
 replace_value(literal::Number) = literal
