cd890181220f1087174df9b84a000d5281948b5e
Hongyang Zhou <hyzhou@umich.edu>
2025-05-05 12:28:22 -0400

    Add precompilation workflow

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,20 +1,20 @@
 # Precompiling workloads
+using PrecompileTools: @setup_workload, @compile_workload
 
 @setup_workload begin
    @compile_workload begin
       # Get a minimal file for precompilation!
-      file = "z=0_raw_1_t25.60000_n00000258.out"
-      bd = load(joinpath(datapath, file))
-      plotrange = [-10.0, 10.0, -Inf, Inf]
-      x, y, w = interp2d(bd, "rho", plotrange)
+      file = joinpath(@__DIR__, "../test/precompile.out")
+      bd = file |> load
+      x, y, w = interp2d(bd, "rho")
       Batsrus.fill_vector_from_scalars(bd, :B)
       get_magnitude(bd, :B)
       get_magnitude2(bd, :B)
       # Linear interpolation at a given point
       d = interp1d(bd, "rho", Float32[0.0, 0.0])
       # Linear interpolation along a line
-      point1 = Float32[-10.0, -1.0]
-      point2 = Float32[10.0, 1.0]
+      point1 = Float32[-1.0, -1.0]
+      point2 = Float32[1.0, 1.0]
       w = interp1d(bd, "rho", point1, point2)
       w = slice1d(bd, "rho", 1, 1)
       get_var_range(bd, "rho")
