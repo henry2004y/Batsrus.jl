9e27d420fa85803bb5aaf086d2ed0117cfeb121d
Hongyang Zhou <hyzhou@umich.edu>
2025-05-05 13:10:51 -0400

    Precompile basic workflow (#78)
    * Add precompilation workflow
    
    * Ref interp1d in get_timeseries
    
    * Bump patch

---
+++ src/precompile.jl
@@ -0,0 +1,22 @@
+# Precompiling workloads
+using PrecompileTools: @setup_workload, @compile_workload
+
+@setup_workload begin
+	@compile_workload begin
+		# Get a minimal file for precompilation!
+		file = joinpath(@__DIR__, "../test/precompile.out")
+		bd = file |> load
+		x, y, w = interp2d(bd, "rho")
+		Batsrus.fill_vector_from_scalars(bd, :B)
+		get_magnitude(bd, :B)
+		get_magnitude2(bd, :B)
+		# Linear interpolation at a given point
+		d = interp1d(bd, "rho", Float32[0.0, 0.0])
+		# Linear interpolation along a line
+		point1 = Float32[-1.0, -1.0]
+		point2 = Float32[1.0, 1.0]
+		w = interp1d(bd, "rho", point1, point2)
+		w = slice1d(bd, "rho", 1, 1)
+		get_var_range(bd, "rho")
+	end
+end
