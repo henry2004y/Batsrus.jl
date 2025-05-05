7ef39922fdac5eae0b2675e8d3765b13caca7fff
Hongyang Zhou <hyzhou@umich.edu>
2025-05-05 12:28:47 -0400

    Ref interp1d in get_timeseries

--- src/select.jl
+++ src/select.jl
@@ -322,7 +322,7 @@ end
 	 get_timeseries(files::AbstractArray, loc; tstep = 1.0)
 
 Extract plasma moments and EM field from PIC output `files` at `loc` with nearest neighbor.
-Currently only works for 2D outputs.
+Currently only works for 2D outputs. If a single point variable is needed, see [`interp1d`](@ref).
 """
 function get_timeseries(files::AbstractArray, loc; tstep = 1.0)
 	nfiles = length(files)
