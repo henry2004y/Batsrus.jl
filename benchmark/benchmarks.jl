a9512920617ab73c97e7a86579a0710d2e222471
Hongyang Zhou <hyzhou@umich.edu>
2026-02-08 22:17:45 -0500

    Remove density extraction from benchmark as it's allocation-free

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -38,7 +38,6 @@ SUITE["read"]["ASCII"] = @benchmarkable load($file)
 file = joinpath(directory, files[2])
 bd = load(file)
 SUITE["read"]["Load binary structured"] = @benchmarkable load($file)
-SUITE["read"]["Extract density"] = @benchmarkable Batsrus.getvar($bd, "rho")
 SUITE["read"]["Extract Bmag"] = @benchmarkable get_magnitude($bd, :B)
 SUITE["read"]["Interp2d"] = @benchmarkable Batsrus.interp2d($bd, "rho")
 
